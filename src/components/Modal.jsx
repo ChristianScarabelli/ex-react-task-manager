02883393c08a7eebf931ed54d0776ccd71e933aa
ChristianScarabelli <scachmabol@hotmail.it>
2025-03-14 11:23:58 +0100

    ADD: milestone 9

---
+++ src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import { createPortal } from "react-dom"
+
+export default function Modal({
+    title, content, show = false, onClose, onConfirm, confirmText = 'Confirm' }) {
+
+    // Funzione per chiudere la modale con la futura funzione passata come prop
+    const handleOnClose = () => {
+        onClose()
+    }
+
+    // Funzione per confermare la modale con la futura funzione passata come prop
+    const handleOnConfirm = () => {
+        onConfirm()
+        handleOnClose()
+    }
+
+
+    return show && createPortal(
+        <div className="fixed inset-0 flex justify-center items-center z-50">
+            <div className="fixed inset-0 bg-black opacity-50 z-40"></div> {/* Overlay */}
+            <div className="relative flex flex-col gap-5 p-8 rounded-lg bg-gray-800 shadow-2xl z-50"> {/* Modale */}
+                <h2 className="text-2xl font-bold text-gray-300">{title}</h2>
+                <p className='text-center text-gray-300'>{content}</p>
+                <div className="flex justify-between items-center">
+                    <button onClick={handleOnClose} className="cursor-pointer text-gray-800 text-sm px-4 py-2 rounded-lg bg-gray-300 hover:bg-gray-400">Cancel</button>
+                    <button onClick={handleOnConfirm} className="cursor-pointer ml-4 text-sm px-4 py-2 rounded-lg bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-green-600">{confirmText}</button>
+                </div>
+            </div>
+        </div>,
+        document.body
+    )
+}
--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,10 +1,14 @@
 import { useNavigate, useParams } from "react-router-dom"
-import { useContext } from "react"
+import { useState, useContext } from "react"
 import GlobalContext from '../contexts/GlobalContext.jsx'
+import Modal from "../components/Modal.jsx"
 export default function TaskDetail() {
     const { id } = useParams()
     const { tasks, removeTask, fetchTasks } = useContext(GlobalContext)
     const navigate = useNavigate()
+
+    // Stato per lo stato di apertura della modale
+    const [showModal, setShowModal] = useState(false)
 
     // Id numerico della task corrente
     const parsedId = parseInt(id)
@@ -12,18 +16,26 @@ export default function TaskDetail() {
     // Recupero la task dal context con id corrispondente al path
     const task = tasks.find((task) => task.id === parsedId)
 
+    // Funzione per gestire il bottone di eliminazione che apre la modale
+    const handleDeleteClick = () => {
+        setShowModal(true)
+    }
 
-    // Funzione per gestire il bottone di eliminazione
-    const handleClick = async (e) => {
-        e.preventDefault()
+    // Funzione per gestire il bottone di chiusura della modale
+    const handleCloseModal = () => {
+        setShowModal(false)
+    }
+
+    // Funzione per gestire il bottone di eliminazione della modale
+    const handleConfirmDelete = async () => {
         try {
             await removeTask(parsedId)
             await fetchTasks()
-            alert('Task deleted succesfully')
+            alert('Task deleted successfully')
             navigate('/tasks')
         }
         catch (err) {
-            alert(`Failed to delete task: ${error.message}`)
+            alert(`Failed to delete task: ${err.message}`)
         }
     }
 
@@ -36,8 +48,16 @@ export default function TaskDetail() {
                 <p><strong>Description:</strong>  {task.description}</p>
                 <span><strong>Status:</strong>  {task.status}</span>
                 <span><strong>Date of creation:</strong>  {task.createdAt}</span>
-                <button onClick={handleClick} className="bg-red-500 hover:bg-red-600 cursor-pointer ms-auto px-4 py-2 rounded-lg">Delete</button>
+                <button onClick={handleDeleteClick} className="bg-red-500 text-gray-100 hover:bg-red-600 cursor-pointer ms-auto px-4 py-2 rounded-lg">Delete</button>
             </div>
-        </section>
+            <Modal
+                title={`Deleting task "${task.title}"`}
+                content='Are you sure to delete this task?'
+                show={showModal}
+                onClose={handleCloseModal}
+                onConfirm={handleConfirmDelete}
+                confirmText='Delete'
+            />
+        </section >
     )
 }
